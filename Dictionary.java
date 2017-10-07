1e7bf2edf04bfc8477c612825f1eb02d92771485
illusionaire <cr4zyb0red@airbears2-10-142-51-93.airbears2.1918.berkeley.edu>
2017-10-07 14:07:43 -0700

    updated websites

--- Dictionary.java
+++ Dictionary.java
@@ -4,8 +4,8 @@ public class Dictionary() {
 	private Set<String> buzzwords;
 
 	public Dictionary() {
-		websites = new HashSet<>();
-		buzzwords = new HashSet<>();
+		websites = new HashSet<String>(getAllWebsites());
+		buzzwords = new HashSet<String>(getAllBuzzwords());
 	}
 
 	public Set<String> getWebsites() {
@@ -14,5 +14,16 @@ public class Dictionary() {
 
 	public Set<String> getBuzzwords() {
 		return this.buzzwords;
+	}
+
+	public List<String> getAllWebsites() {
+		return Arrays.asList("https://www.nytimes.com/topic/subject/stocks-and-bonds",
+							 "https://www.fool.com/",
+							 "https://www.nytimes.com/topic/organization/new-york-stock-exchange",
+							 "http://www.msn.com/en-us/money/investing");
+	}
+
+	public List<String> getAllBuzzwords() {
+		return Arrays.asList();
 	}
 }
